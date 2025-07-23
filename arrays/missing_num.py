398581796a0a9299f49b5de9f2f4cc62d9ffc720
Durgaprasad vulla <durgaprasadvulla24@gmail.com>
2025-07-23 05:43:56 +0530

    Update missing_num.py

--- arrays/missing_num.py
+++ arrays/missing_num.py
@@ -2,3 +2,9 @@ nums = list(map(int,input("enter array elements :").split()))
 for i in range(len(nums)+1):
         if i not in nums:
                 print(i)
+
+        # optimal
+        #         n = len(nums)
+        # expected_sum = n * (n + 1) // 2
+        # actual_sum = sum(nums)
+        # return expected_sum - actual_sum
